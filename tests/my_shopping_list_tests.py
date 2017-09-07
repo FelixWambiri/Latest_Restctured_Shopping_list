8a11523b52fe76b054811d57a35f070f50e15cd2
felix <felixwambiri21@gmail.com>
2017-09-06 22:13:41 -0700

    update my tests

--- tests/my_shopping_list_tests.py
+++ tests/my_shopping_list_tests.py
@@ -9,18 +9,18 @@ class TestMyShoppingList(unittest.TestCase):
         myshopinglist = ShoppingList("Somalia",
                                        "This is the shopping list of items i bought in somalia while on a trip there.")
         item = Item("Laptop", "80000", "2 pieces")
-        self.assertEqual(0, len(myshopinglist.shopping_list))
+        self.assertEqual(0, len(myshopinglist.purchased_items))
         myshopinglist.add_item(item)
 
     def test_you_can_add_multiple_items(self):
-        myshopinglist = ShoppingList("Somalia", "shopping list of items i bought in somalia.")
+        myshoppinglist = ShoppingList("Somalia", "shopping list of items i bought in somalia.")
         item = Item("Laptop", "80000", "2 pieces")
         item2 = Item("Laptop2", "80000", "2 pieces")
         item3 = Item("Laptop3", "80000", "2 pieces")
-        myshopinglist.add_item(item)
-        myshopinglist.add_item(item2)
-        myshopinglist.add_item(item3)
-        self.assertEqual(3, len(myshopinglist.shopping_list))
+        myshoppinglist.add_item(item)
+        myshoppinglist.add_item(item2)
+        myshoppinglist.add_item(item3)
+        self.assertEqual(3, len(myshoppinglist.purchased_items))
 
 
 if __name__ == '__main__':
