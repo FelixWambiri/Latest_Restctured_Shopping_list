72f4e3cfc786ef688605c294b05ddaabb8fac638
felix <felixwambiri21@gmail.com>
2017-09-07 04:14:19 -0700

    modify wtf form to add items

--- Develop/ClassModels/additemform.py
+++ Develop/ClassModels/additemform.py
@@ -1,9 +1,9 @@
 from flask_wtf import Form
 from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, length
 
 
 class AddItemsManager(Form):
-    name = StringField('name', [DataRequired(), Length(min=4, max=25)])
-    price = StringField('price', [DataRequired(), Length(min=4, max=25)])
-    quantity = StringField('quantity'[DataRequired(), Length(min=4, max=25)])
+    name = StringField('name', [DataRequired(), length(min=4, max=25)])
+    price = StringField('price', [DataRequired(), length(min=4, max=25)])
+    quantity = StringField('quantity', [DataRequired(), length(min=4, max=25)])
